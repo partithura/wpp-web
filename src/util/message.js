2b5f14cd00077c968d14d9ee61f5c63bdd3824a2
Juliano Stroschon <julianostroschon@gmail.com>
2023-07-12 16:49:51 -0300

    🔇 Removido log

--- src/util/message.js
+++ src/util/message.js
@@ -1,6 +1,7 @@
 const { DateTime } = require('luxon');
 const { readFile, writeFile } = require('fs/promises');
 
+const SERVICES_TO_MUTE = ['datasync'];
 const FILENAME = 'messages.json';
 const DATE_FORMAT = "T' 'dd/MM/yy";
 const NO_MESSAGES = [];
@@ -8,13 +9,11 @@ const HOUR_NOTIFY = {
   MINIMUM: 6,
   MAXIMUM: 22,
 };
-const SERVICES_TO_MUTE = ['datasync'];
 
 function isSilentHour() {
   const hour = DateTime.now().setZone('America/Sao_Paulo').get('hour');
   return hour <= HOUR_NOTIFY.MINIMUM || hour >= HOUR_NOTIFY.MAXIMUM;
 }
-console.log(isSilentHour());
 
 function getFilePath() {
   return `${__dirname}/${FILENAME}`;
