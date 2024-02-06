cf0f3af538ee69d73dcb8cd83d4c1b85d02beec4
Leo Bueno <leobueno.dev@gmail.com>
2024-02-06 13:33:25 -0300

    🔥 ajustes na porta

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { buildServer } = require('./src/server');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 (async () => {
   const server = await buildServer();
