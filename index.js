3683ec97bf0e836811c4edee4c1727639557f3a8
Leo Bueno <leobueno.dev@gmail.com>
2024-02-06 12:09:00 -0300

    🔥 localhost run

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 const { buildServer } = require('./src/server');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 (async () => {
   const server = await buildServer();
 
-  server.listen(PORT);
+  server.listen(PORT, '0.0.0.0');
 })();
